d670048a0dd42eeaf03f662ec146ae1d64a0d274
elandau <elandau@netflix.com>
2018-09-16 22:29:24 -0700

    Fix unit test

--- concurrency-limits-core/src/test/java/com/netflix/concurrency/limits/limit/ExpAvgMeasurementTest.java
+++ concurrency-limits-core/src/test/java/com/netflix/concurrency/limits/limit/ExpAvgMeasurementTest.java
@@ -1,17 +1,20 @@
 package com.netflix.concurrency.limits.limit;
 
 import com.netflix.concurrency.limits.limit.measurement.ExpAvgMeasurement;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class ExpAvgMeasurementTest {
     @Test
-    public void test() {
-        ExpAvgMeasurement avg = new ExpAvgMeasurement(10);
+    public void testWarmup() {
+        ExpAvgMeasurement avg = new ExpAvgMeasurement(10, 10, Math::min);
 
         for (int i = 0; i < 10; i++) {
-            avg.add(1);
+            avg.add(i + 10);
+            Assert.assertEquals(10.0, avg.get().doubleValue(), 0.01);
         }
 
-        avg.add(10);
+        avg.add(100);
+        Assert.assertEquals(26.36, avg.get().doubleValue(), 0.01);
     }
 }
